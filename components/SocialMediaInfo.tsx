bb62cc9921cc8ae257ba829266e957e9c14548b2
igornfaustino <igor@nfaustino.com>
2021-11-28 20:09:41 -0300

    feat: add animation to social link items

--- components/SocialMediaInfo.tsx
+++ components/SocialMediaInfo.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -5,28 +6,52 @@ const Wrapper = styled.div`
 	gap: 32px;
 `;
 
-const Links = styled.a`
+const Links = styled(motion.a)`
 	cursor: pointer;
 	text-decoration: none;
 	color: ${({ theme }) => theme.colors.textColor};
+
+	padding-bottom: 4px;
+	border-bottom: 1px solid transparent;
+
+	:hover {
+		border-bottom: 1px solid ${({ theme }) => theme.colors.textColor};
+	}
 `;
+
+const hoverAnimation = {
+	scale: 1.1,
+};
 
 const SocialMediaInfo = function () {
 	return (
 		<Wrapper>
 			<Links
+				whileHover={hoverAnimation}
 				href="https://github.com/igornfaustino/nfaustino.com"
 				target="_blank"
 			>
 				Source
 			</Links>
-			<Links href="https://www.linkedin.com/in/igornfaustino" target="_blank">
+			<Links
+				whileHover={hoverAnimation}
+				href="https://www.linkedin.com/in/igornfaustino"
+				target="_blank"
+			>
 				Linkedin
 			</Links>
-			<Links href="https://github.com/igornfaustino" target="_blank">
+			<Links
+				whileHover={hoverAnimation}
+				href="https://github.com/igornfaustino"
+				target="_blank"
+			>
 				Github
 			</Links>
-			<Links href="https://twitter.com/igornfaustino" target="_blank">
+			<Links
+				whileHover={hoverAnimation}
+				href="https://twitter.com/igornfaustino"
+				target="_blank"
+			>
 				Twitter
 			</Links>
 		</Wrapper>
