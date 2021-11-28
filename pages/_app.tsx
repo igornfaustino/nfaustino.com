3c78c8d0101d00102c54c457b3efb79e06224a00
igornfaustino <igor@nfaustino.com>
2021-11-28 16:54:05 -0300

    feat: setup styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/global";
+import defaultTheme from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/theme.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+  colors: {},
+} as const;
+
+export type ThemeType = typeof defaultTheme;
+
+export default defaultTheme;
