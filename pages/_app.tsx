970e13c2575f85c1d8dc2f14db832cbfd693fc67
igornfaustino <igor@nfaustino.com>
2022-03-01 16:00:57 -0300

    feat: get posts from cms

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,37 @@
 import { DefaultSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
+import { Provider } from 'urql';
 
 import Analytics from '../components/atoms/Analytics';
 import Spotlight from '../components/atoms/Spotlight';
 import CustomThemeProvider from '../contexts/CustomThemeProvider';
+import { client, ssrCache } from '../lib/urql';
 import { GlobalStyle } from '../styles/global';
 
 const MyApp = function ({ Component, pageProps }: AppProps) {
+	if (pageProps.urqlState) {
+		ssrCache.restoreData(pageProps.urqlState);
+	}
+
 	return (
-		<CustomThemeProvider>
-			<DefaultSeo
-				openGraph={{
-					type: 'website',
-					url: 'https://nfaustino.com',
-					site_name: 'Igor n Faustino',
-				}}
-				titleTemplate="%s | Igor n Faustino"
-				defaultTitle="Igor n Faustino"
-			/>
-			<Spotlight>
-				<GlobalStyle />
-				<Component {...pageProps} />
-				<Analytics />
-			</Spotlight>
-		</CustomThemeProvider>
+		<Provider value={client}>
+			<CustomThemeProvider>
+				<DefaultSeo
+					openGraph={{
+						type: 'website',
+						url: 'https://nfaustino.com',
+						site_name: 'Igor n Faustino',
+					}}
+					titleTemplate="%s | Igor n Faustino"
+					defaultTitle="Igor n Faustino"
+				/>
+				<Spotlight>
+					<GlobalStyle />
+					<Component {...pageProps} />
+					<Analytics />
+				</Spotlight>
+			</CustomThemeProvider>
+		</Provider>
 	);
 };
 
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -6,7 +6,8 @@ import { Title } from '../components/atoms/Title';
 import { BlogList } from '../components/organisms/BlogList';
 import useSpotlightActions from '../hooks/useSpotlightActions';
 import BaseLayout from '../layouts/BaseLayout';
-import { getAllPostsMetadata, PostMetadata } from '../lib/post';
+import { client, ssrCache } from '../lib/urql';
+import { AllPostsDocument, useAllPostsQuery } from '../src/generated/graphql';
 
 const Wrapper = styled.div`
 	display: flex;
@@ -21,14 +22,26 @@ const Wrapper = styled.div`
 	}
 `;
 
-type Props = {
-	posts: PostMetadata[];
+export const getPostReadingTime = (content: string) => {
+	const wpm = 225;
+	const words = content.trim().split(/\s+/).length;
+	const time = Math.ceil(words / wpm);
+
+	return time;
 };
 
-const Blog: NextPage<Props> = function ({ posts }) {
+const Blog: NextPage = function () {
 	const pageDescription = `Here you will be able to find all posts I wrote. 
 	You can find posts about development, productivity, and everything that's in my mind. 
 	Posts are in both Portuguese and English. Hope you like :)`;
+
+	const [{ data }] = useAllPostsQuery();
+
+	const posts =
+		data?.posts.map((post) => ({
+			...post,
+			readingTime: getPostReadingTime(post.content),
+		})) || [];
 
 	useSpotlightActions(posts);
 
@@ -49,12 +62,12 @@ const Blog: NextPage<Props> = function ({ posts }) {
 	);
 };
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
-	const posts = await getAllPostsMetadata();
+export const getStaticProps: GetStaticProps = async () => {
+	await client.query(AllPostsDocument).toPromise();
 
 	return {
 		props: {
-			posts,
+			urqlState: ssrCache.extractData(),
 		},
 	};
 };
