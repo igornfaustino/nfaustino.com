bfa6d3ba0079c4fe29fd6a841fbcab02638db0f9
igornfaustino <igor@nfaustino.com>
2022-03-01 22:49:39 -0300

    feat: get project page data from cms

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,10 +1,17 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
+import { NextSeo } from "next-seo";
 import styled from "styled-components";
 
 import HyperLink from "../components/atoms/HipperLink";
+import Markdown from "../components/atoms/Markdown";
 import { Title } from "../components/atoms/Title";
 import { ProjectList } from "../components/organisms/ProjectList";
+import {
+  GetPageByUrlDocument,
+  useGetPageByUrlQuery,
+} from "../generated/graphql";
 import BaseLayout from "../layouts/BaseLayout";
+import { client, ssrCache } from "../lib/urql";
 
 const Wrapper = styled.div`
   display: flex;
@@ -20,24 +27,38 @@ const Wrapper = styled.div`
 `;
 
 const Projects: NextPage = function () {
+  const [{ data }] = useGetPageByUrlQuery({ variables: { url: "projects" } });
+
   return (
     <BaseLayout>
+      <NextSeo
+        title={data?.page?.title}
+        description={data?.page?.description || ""}
+      />
+
       <Wrapper>
-        <Title>Projects</Title>
-        <p>
-          In this page I shared some of my cool projects I worked since 2017.
-          The most of this projects can be found in my{" "}
-          <HyperLink href="https://github.com/igornfaustino" target="_blank">
-            Github
-          </HyperLink>
-          . The projects are listed in a <b>desc</b> order
-        </p>
+        <Title>{data?.page?.title}</Title>
+        <Markdown content={data?.page?.description || ""} />
+
         <hr style={{ width: "100%" }} />
 
         <ProjectList />
       </Wrapper>
     </BaseLayout>
   );
+};
+
+const MINUTE = 60;
+
+export const getStaticProps: GetStaticProps = async () => {
+  await client.query(GetPageByUrlDocument, { url: "projects" }).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+    revalidate: 5 * MINUTE,
+  };
 };
 
 export default Projects;
