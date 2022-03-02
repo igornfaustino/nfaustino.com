d9c209e2173c7f5df61dc89a7a027e6343f42521
igornfaustino <igor@nfaustino.com>
2022-03-01 22:59:57 -0300

    feat: get podcacst page data from cms

--- src/pages/podcast.tsx
+++ src/pages/podcast.tsx
@@ -1,11 +1,16 @@
+import { NextSeo } from "next-seo";
 import { parse } from "rss-to-json";
 import styled from "styled-components";
+import Markdown from "../components/atoms/Markdown";
 
 import { Title } from "../components/atoms/Title";
 import { PodcastList } from "../components/organisms/PodcastList";
+import {
+  GetPageByUrlDocument,
+  useGetPageByUrlQuery,
+} from "../generated/graphql";
 import BaseLayout from "../layouts/BaseLayout";
-
-const HOUR = 60 * 60;
+import { client, ssrCache } from "../lib/urql";
 
 const Wrapper = styled.div`
   display: flex;
@@ -29,16 +34,19 @@ type Props = {
 };
 
 const Podcast = function ({ episodes }: Props) {
+  const [{ data }] = useGetPageByUrlQuery({ variables: { url: "podcast" } });
+
   return (
     <BaseLayout>
+      <NextSeo
+        title={data?.page?.title}
+        description={data?.page?.description || ""}
+      />
+
       <Wrapper>
-        <Title>Podcast</Title>
-        <p>
-          Here you will be able to find all episodes of my podcast. The episodes
-          are about programming and productivity. Me and a friend uses this
-          podcast to share knowledge with others. (All the episodes are in
-          Portuguese )
-        </p>
+        <Title>{data?.page?.title}</Title>
+        <Markdown content={data?.page?.description || ""} />
+
         <hr style={{ width: "100%" }} />
 
         <PodcastList episodes={episodes} />
@@ -47,14 +55,18 @@ const Podcast = function ({ episodes }: Props) {
   );
 };
 
+const MINUTE = 60;
+
 export const getStaticProps = async () => {
   const rss = await parse("https://anchor.fm/s/157bfc7c/podcast/rss", {});
+  await client.query(GetPageByUrlDocument, { url: "podcast" }).toPromise();
 
   return {
     props: {
+      urqlState: ssrCache.extractData(),
       episodes: JSON.parse(JSON.stringify(rss.items, null, 3)),
     },
-    revalidate: 6 * HOUR,
+    revalidate: 30 * MINUTE,
   };
 };
 
